ed1d4cd9d874a7bb256160340ea85e003c2968e0
iantendu <iantendu@gmail.com>
2021-12-10 16:12:35 +0300

    first commit

---
+++ app/src/main/java/com/example/stateflow/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.stateflow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.plcoding.stateflow.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
